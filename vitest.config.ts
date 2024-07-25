d76b331723e1831ec1893c5e85239f9efe593f74
Marco Gullo <marco.gullo@apify.com>
2024-07-25 09:45:23 +0200

    setup tests

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    plugins: [tsconfigPaths()],
+    esbuild: {
+        target: 'es2022',
+        keepNames: true,
+    },
+    test: {
+        globals: true,
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'lcov', 'cobertura'],
+            exclude: [
+                '**/node_modules/**',
+                '**/dist/**',
+                '**/test/**',
+            ],
+        },
+        restoreMocks: true,
+        testTimeout: 60_000,
+        hookTimeout: 60_000,
+    },
+});

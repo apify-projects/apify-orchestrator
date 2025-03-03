cee38e7965996908286418631d25fae32f1aea4b
Marco Gullo <marco.gullo@apify.com>
2025-03-03 13:22:59 +0100

    add test for actor-client.start

--- test/clients/actor-client.test.ts
+++ test/clients/actor-client.test.ts
@@ -1,4 +1,40 @@
+import { ActorRun, ApifyClient } from 'apify-client';
+import { EnqueueFunction, ExtActorClient, ForcedEnqueueFunction } from 'src/clients/actor-client.js';
+import { RunsTracker } from 'src/tracker.js';
+import { CustomLogger } from 'src/utils/logging.js';
+import { Mock } from 'vitest';
+
 describe('actor-client methods', () => {
+    let customLogger: CustomLogger;
+    let runsTracker: RunsTracker;
+    let enqueueFunction: Mock<EnqueueFunction>;
+    let forcedEnqueueFunction: ForcedEnqueueFunction;
+
+    const mockDate = new Date('2024-09-11T06:00:00.000Z');
+
+    const getMockRun = (id: string, status = 'READY', defaultDatasetId = 'test-dataset-id') => {
+        return {
+            id,
+            status,
+            defaultDatasetId,
+            startedAt: mockDate,
+        } as ActorRun;
+    };
+
+    beforeEach(async () => {
+        vi.useFakeTimers();
+        customLogger = new CustomLogger(false, false);
+        runsTracker = new RunsTracker(customLogger, false);
+        await runsTracker.init();
+        enqueueFunction = vi.fn();
+        forcedEnqueueFunction = vi.fn();
+    });
+
+    afterEach(() => {
+        vi.useRealTimers();
+        vi.resetAllMocks();
+    });
+
     describe('start', () => {
         it('returns an existing Run, if already available', async () => {
             // TODO: test
@@ -10,6 +46,42 @@ describe('actor-client methods', () => {
 
         it('enqueues a new request, if an existing Run was not found', async () => {
             // TODO: test
+        });
+
+        it('enqueues a new request with the fixed input, if defined', async () => {
+            const fixedInput = { fixedKey: 'fixedValue' };
+
+            const mockRun = getMockRun('test-run-id');
+
+            enqueueFunction = vi.fn(({ startCallbacks }) => {
+                startCallbacks[0](mockRun);
+            });
+
+            const sdkApifyClient = new ApifyClient();
+            const sdkActorClient = sdkApifyClient.actor('test-actor-id');
+            const actorClient = new ExtActorClient(
+                sdkActorClient,
+                customLogger,
+                runsTracker,
+                enqueueFunction,
+                forcedEnqueueFunction,
+                fixedInput,
+            );
+
+            const run = await actorClient.start('test-run', { someKey: 'someValue' });
+
+            expect(enqueueFunction).toBeCalledWith({
+                runName: 'test-run',
+                defaultMemoryMbytes: expect.anything(),
+                input: {
+                    someKey: 'someValue',
+                    fixedKey: 'fixedValue',
+                },
+                options: undefined,
+                startCallbacks: expect.anything(),
+                startRun: expect.anything(),
+            });
+            expect(run).toEqual(mockRun);
         });
     });
 

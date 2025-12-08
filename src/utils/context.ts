0ba5259edd27143d3111f58e60140e8478a93678
Marco Gullo <marco.gullo@apify.com>
2025-12-08 10:35:47 +0100

    doc(context): mark OrchestratorContext as deprecated

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -2,6 +2,10 @@ import type { RunTracker } from '../tracking/run-tracker.js';
 import type { Logger } from './logging.js';
 import type { Storage } from './storage.js';
 
+/**
+ * @deprecated `runTracker` should not be in the global context, because there is one tracker per Apify client.
+ * TODO: Remove or replace.
+ */
 export interface OrchestratorContext {
     logger: Logger;
     runTracker: RunTracker;

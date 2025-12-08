529fb2c4bad50bb601690c1ad3c90cdb4c8eff9e
Marco Gullo <marco.gullo@apify.com>
2025-12-08 13:34:16 +0100

    fix: key-value-store test mocks

--- test/utils/key-value-store.test.ts
+++ test/utils/key-value-store.test.ts
@@ -1,9 +1,21 @@
-import { Actor, KeyValueStore } from 'apify';
+import { Actor } from 'apify';
 import { encryptString, processEncryptionKey } from 'src/utils/encryption.js';
 import { EncryptedKeyValueStore } from 'src/utils/key-value-store.js';
 import { buildLogger } from 'src/utils/logging.js';
 import { getTestOptions } from 'test/_helpers/context.js';
-import { describe, expect, it, vi } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+
+vi.mock('apify', async (importActual) => ({
+    ...(await importActual()),
+    Actor: {
+        getValue: vi.fn(),
+        setValue: vi.fn(),
+        on: vi.fn(),
+        config: {
+            get: vi.fn(),
+        },
+    },
+}));
 
 describe('EncryptedKeyValueStore', () => {
     const options = getTestOptions();
@@ -12,31 +24,33 @@ describe('EncryptedKeyValueStore', () => {
     const secret = 'test-encryption-key';
     const encryptionKey = processEncryptionKey(secret);
 
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
     describe('useState', () => {
         it('restores state correctly', async () => {
             const testValue = { counter: 42 };
 
-            const getValueSpy = vi
-                .spyOn(KeyValueStore.prototype, 'getValue')
-                .mockResolvedValue(encryptString(JSON.stringify(testValue), encryptionKey));
+            vi.mocked(Actor.getValue).mockResolvedValue(encryptString(JSON.stringify(testValue), encryptionKey));
 
             const kvs = new EncryptedKeyValueStore(logger, encryptionKey);
             const testKey = 'TEST_KEY';
 
             const state = await kvs.useState<{ counter: number }>(testKey, { counter: 0 });
-            expect(getValueSpy).toHaveBeenCalledWith(testKey);
+            expect(Actor.getValue).toHaveBeenCalledWith(testKey);
             expect(state).toEqual(testValue);
         });
 
         it('returns default value when key does not exist', async () => {
-            const getValueSpy = vi.spyOn(KeyValueStore.prototype, 'getValue').mockResolvedValue(null);
+            vi.mocked(Actor.getValue).mockResolvedValueOnce(null);
 
             const kvs = new EncryptedKeyValueStore(logger, encryptionKey);
             const testKey = 'NON_EXISTENT_KEY';
             const defaultValue = { message: 'Hello, World!' };
 
             const state = await kvs.useState<typeof defaultValue>(testKey, defaultValue);
-            expect(getValueSpy).toHaveBeenCalledWith(testKey);
+            expect(Actor.getValue).toHaveBeenCalledWith(testKey);
             expect(state).toEqual(defaultValue);
         });
 
@@ -44,20 +58,22 @@ describe('EncryptedKeyValueStore', () => {
             const anotherEncryptionKey = processEncryptionKey('another-encryption-key');
             const invalidEncryptedData = encryptString(JSON.stringify({ data: 'test' }), anotherEncryptionKey);
 
-            const getValueSpy = vi.spyOn(KeyValueStore.prototype, 'getValue').mockResolvedValue(invalidEncryptedData);
+            vi.mocked(Actor.getValue).mockResolvedValueOnce(invalidEncryptedData);
 
             const kvs = new EncryptedKeyValueStore(logger, encryptionKey);
             const testKey = 'INVALID_ENCRYPTED_KEY';
             const defaultValue = { message: 'Hello, World!' };
 
             const state = await kvs.useState<typeof defaultValue>(testKey, defaultValue);
-            expect(getValueSpy).toHaveBeenCalledWith(testKey);
+            expect(Actor.getValue).toHaveBeenCalledWith(testKey);
             expect(state).toEqual(defaultValue);
         });
 
         it('returns the cached value on subsequent calls', async () => {
             const testValue = { counter: 100 };
             const testKey = 'CACHED_KEY';
+
+            vi.mocked(Actor.getValue).mockResolvedValue(encryptString(JSON.stringify(testValue), encryptionKey));
 
             const kvs = new EncryptedKeyValueStore(logger, encryptionKey);
 
@@ -67,33 +83,33 @@ describe('EncryptedKeyValueStore', () => {
 
             const state2 = await kvs.useState(testKey, { counter: 0 });
             expect(state2).toEqual({ counter: 101 });
+            expect(Actor.getValue).toHaveBeenCalledTimes(1);
         });
 
         it('persists state on corresponding event', async () => {
             const testValue = { counter: 7 };
             const testKey = 'PERSIST_KEY';
 
-            const setValueSpy = vi.spyOn(KeyValueStore.prototype, 'setValue');
-
             let persistCallback: () => Promise<void>;
 
-            const onSpy = vi.spyOn(Actor, 'on').mockImplementationOnce((_event, callback) => {
+            vi.mocked(Actor.on).mockImplementationOnce((_event, callback) => {
                 persistCallback = callback as () => Promise<void>;
             });
+            vi.mocked(Actor.config.get).mockReturnValue(10_000);
 
             const kvs = new EncryptedKeyValueStore(logger, encryptionKey);
 
-            expect(onSpy).toHaveBeenCalledWith('persistState', expect.any(Function));
+            expect(Actor.on).toHaveBeenCalledWith('persistState', expect.any(Function));
 
             await kvs.useState(testKey, testValue);
 
             await persistCallback();
 
-            expect(setValueSpy).toHaveBeenCalledWith(
+            expect(Actor.setValue).toHaveBeenCalledWith(
                 testKey,
                 expect.any(String),
                 expect.objectContaining({
-                    timeoutSecs: expect.any(Number),
+                    timeoutSecs: 5,
                 }),
             );
         });
